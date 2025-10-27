0e7418de4b70e27d428649c8dd1610546a4cb10a
pleyvaramirez <156385234+pleyvaramirez@users.noreply.github.com>
2025-10-26 18:03:45 -0700

    QuestionBox.ts file added (not implemented)

---
+++ src/views/QuestionBox.ts
@@ -0,0 +1,14 @@
+// src/views/QuestionBox.ts
+// 
+// 
+/*=============================
+  VIEW LAYER:
+  TODO: add description here
+  - 
+
+==============================*/
+// Use Konva to draw a question box
+
+import Konva from "konva";
+import { MCQ, MCQStatus } from "../models/Question";
+
