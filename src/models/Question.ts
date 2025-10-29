5641b5ec2f4be4c275be1c2bf4d5c5c973f06b6d
pleyvaramirez <156385234+pleyvaramirez@users.noreply.github.com>
2025-10-28 23:47:41 -0700

    Converted Question from type to class for better state handling

--- src/models/Question.ts
+++ src/models/Question.ts
@@ -8,28 +8,68 @@
       Type: 
 */
 
+import { USState } from "./State";
+
 // Exposed so controllers/views can import and use them.
-export enum MCQStatus {
+export enum QuestionStatus {
     Unanswered = "Unanswered", 
     Correct    = "Correct",
     Incorrect  = "Incorrect"
+    // Skipped = "Skipped"  ...  If we want to give players the power to skip certain questions entirely, e.g. like a skip "power-up".
+}
+
+export enum QuestionType {
+    Capital = "Capital", 
+    Flower  = "Flower",
+    History = "History"
 }
 
 // Domain object for a single question.
-export type MCQ = {
-    code: string;           // e.g. "CA2" for California's second question, "MN1" for Minnesota's first question
-    question: string;       // 
-    correct: Answer;        // Each question will always be assigned the same correct answer. 
-    status: MCQStatus;      // Current question state, function above.
-};
+export class Question {
+    public readonly state: USState;                // the US state this question is about.
+    public readonly which: QuestionType;           // Can be capital, flower, history. Check enum above.
+    status: QuestionStatus;                        // Current question state.
+    public readonly questionText: string;          // The question being asked.
+    correctAnswer: Answer;                         // The correct answer. 
+
+    constructor (
+        state: USState, 
+        which: QuestionType, 
+        questionText: string, 
+        correctAnswer: Answer,                      
+    ) {         
+        this.state = state;                                    
+        this.which = which;  
+        this.status = QuestionStatus.Unanswered;
+        this.questionText = questionText;
+        this.correctAnswer = correctAnswer;                     
+    }
+
+    getUSState(): USState {
+        return this.state;
+    }
+
+    getWhichType(): QuestionType {
+        return this.which;
+    }
+
+    getStatus(): QuestionStatus {
+        return this.status;
+    }
+
+    getCorrectAnswer(): Answer {
+        return this.correctAnswer;
+    }
+}
+
 
 export enum AnswerStatus {
-    NotSelected = "NotSelected", 
-    Selected = "Selected"
+    NotSelected = "NotSelected",
+    Selected    = "Selected"
 }
 
 // Domain object for a single answer.
-export type Answer = {
-    answer: string;
+export type Answer = {                   // For flexibility, correctness is not stored in the Answer object itself.
+    answerText: string;
     status: AnswerStatus;
 }
