cadc8c52182f40373d5d355929cc0e0aa93b6e92
Dennis <desun@ucsd.edu>
2025-11-18 22:08:27 -0800

    Update vite.config.ts
    add base repo name for web visit function.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vitest/config";
 export default defineConfig({
+  base: "/cse-110-project-team-4/", // for Github - settings - pages
   test: {
     environment: 'jsdom',
   }
